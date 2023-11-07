3419d84c8651bc2b9f0f9febbdddb1ecefe1315c
nikhilkuyya <nikhilkuyya@gmail.com>
2023-11-07 11:43:13 +0530

    chore: deploy test

--- src/NoStateSearchParams.tsx
+++ src/NoStateSearchParams.tsx
@@ -1,17 +1,30 @@
 const NoStateSearchParams = () => {
-  const location = 'test';
+  const location = "test";
   // to test thre renders
-  console.log('hi, nostate searchParms')
+  console.log("hi, nostate searchParms");
   return (
     <div>
-    <form>
-    <label htmlFor="location">
-    <input id="location"  
-    placeholder="Enter Location" defaultValue={location} />
-    </label>
-    </form>
+      <form>
+        <fieldset>
+          <legend>My Render Test</legend>
+          <label htmlFor="location">
+            <input
+              id="location"
+              placeholder="Enter Location"
+              defaultValue={location}
+            />
+          </label>
+          <label htmlFor="valuelocation">
+            <input
+              id="valuelocation"
+              placeholder="Enter Location"
+              value={location}
+            />
+          </label>
+        </fieldset>
+      </form>
     </div>
   );
-}
+};
 
 export default NoStateSearchParams;
